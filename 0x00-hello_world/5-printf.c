b8d31474ec5fdbc7d9078d6df1db271f297ba693
Emmanuelsam1 <Emmanuelsamuel415@gmail.com>
2023-09-07 16:43:41 -0700

    hello, printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - this is a printf.c project
+ *
+ * Return: 0 when successfull
+ */
+
+int main(void)
+{
+	printf("with power grammer, but the outcome is a piece of art.\n");
+	return (0);
+}
+
