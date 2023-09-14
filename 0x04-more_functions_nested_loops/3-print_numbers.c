14dcfa7b83f0d7554e8fbe7b1f181bf5bb1dc010
Emmanuelsam1 <Emmanuelsamuel415@gmail.com>
2023-09-14 14:10:41 -0700

    speaking numbers

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * print_numbers - print numbers from 0 to 9
+ * print_numbers - prints numbers from 0 to 9
  * Return: void
  */
 
@@ -11,6 +11,6 @@ void print_numbers(void)
 
 	for (i = 0 ; i < 10 ; i++)
 		_putchar(i + '0');
-	
+
 	_putchar('\n');
 }
