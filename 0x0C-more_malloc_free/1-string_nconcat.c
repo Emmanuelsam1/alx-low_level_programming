810d6a8fd2ae2cc74d6303a35494ccd9d7885b01
Emmanuelsam1 <Emmanuelsamuel415@gmail.com>
2023-10-08 16:04:52 -0700

    nconcat

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -25,7 +25,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	for (i = 0; s1[i] != '\0'; i++)
 		s1len++;
 	for (i = 0; s2[i] != '\0'; i++)
-		s2++;
+		s2len++;
 
 	output = malloc(sizeof(char) * (s1len + n) + 1);
 	if (output == NULL)
