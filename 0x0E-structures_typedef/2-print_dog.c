a93afedfbb381d221622bf68b12066d9a6302965
Emmanuelsam1 <Emmanuelsamuel415@gmail.com>
2023-10-10 18:17:04 -0700

    poppy owner

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,24 @@
+#include "dog.h"
+#include <stdio.h>
+
+/**
+ * print_dog - a function that prints struct dog
+ * @d: the new dog type struct
+ */
+void print_dog(struct dog *d)
+{
+	if (d)
+	{
+		if (!((*d).name))
+			printf("name: (nill)\n");
+		else
+			printf("name: %s\n", d->name);
+		printf("Age: %f\n", d->age);
+
+		if (!(d->owner))
+			printf("owner: (nill)\n");
+		else
+			printf("owner: %s\n", (*d).owner);
+
+	}
+}
