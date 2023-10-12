0c4d09b52bedfbe8645dab38a2eb09d6191d0545
Emmanuelsam1 <Emmanuelsamuel415@gmail.com>
2023-10-12 13:50:05 -0700

    task 0

--- 0x10-variadic_functions/variadic_function.h
+++ 0x10-variadic_functions/variadic_function.h
@@ -1,5 +1,9 @@
-#ifndef VARIADIC_FUNCTION_H
-#define VARIADIC_FUNCTION_H
+#ifndef _VARIADIC_FUNCTION_H
+#define _VARIADIC_FUNCTION_H
+
+#include <stdio.h>
+#include <stddef.h>
+#include <stdarg.h>
 
 int sum_them_all(const unsigned int n, ...);
 
